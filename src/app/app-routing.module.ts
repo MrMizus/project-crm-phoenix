432ad8d18233fbf62b19a8baae7707053eeaef81
Mizus <jankowski.michal96@gmail.com>
2023-03-06 21:46:22 +0100

    feat: add validation to register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { VerifyComponent } from './components/verify/verify.component';
 import { LoginComponentModule } from './components/login/login.component-module';
-import { RegisterComponent } from './components/register/register.component';
 import { RegisterComponentModule } from './components/register/register.component-module';
+import { VerifyComponentModule } from './components/verify/verify.component-module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  {
-    path: 'auth',
-    children: [
-      {
-        path: 'login',
-        component: LoginComponent,
-      },
-      {
-        path: 'register',
-        component: RegisterComponent
-      }
-    ],
-  },
+  { path: 'auth',
+  children: [
+    {
+      path: 'login',
+      component: LoginComponent,
+    },
+    {
+      path: 'register',
+      component: RegisterComponent
+    }
+  ], },
+  { path: 'verify', component: VerifyComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), LoginComponentModule, RegisterComponentModule],
+  imports: [RouterModule.forRoot(routes), LoginComponentModule, RegisterComponentModule, VerifyComponentModule],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,19 +1,107 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+export const hasNumberValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  if (!control.value) return null;
+  if (control.value.search(/(?=.*[0-9])/) > -1) return null;
+  return { hasNumber: true };
+};
+
+export const hasSpecialValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  if (!control.value) return null;
+  if (control.value.search(/(?=.*[!@#$%^*()])/) > -1) return null;
+  return { hasSpecial: true };
+};
+
+export const hasCapitalValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  if (!control.value) return null;
+  if (control.value.search(/(?=.*[A-Z])/) > -1) return null;
+  return { hasCapital: true };
+};
+
+export const hasSmallValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  if (!control.value) return null;
+  if (control.value.search(/(?=.*[a-z])/) > -1) return null;
+
+  return { hasSmall: true };
+};
+
+export const isPasswordMatchValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  const password: string = control.get('password')?.value;
+  const confirmPassword: string = control.get('confirmPassword')?.value;
+
+  if (!confirmPassword || !password) return null;
+  if (password !== confirmPassword) {
+    return { isMatch: true };
+  }
+  return null;
+};
 
 @Component({
   selector: 'app-register',
   styleUrls: ['./register.component.scss'],
   templateUrl: './register.component.html',
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterComponent {
-  readonly registerForm: FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]), 
-    comfPassword: new FormControl('', [Validators.required]), 
-    policy: new FormControl('', [Validators.required])
-  });
+  isValid: boolean = false
+  readonly registerForm: FormGroup = new FormGroup(
+    {
+      name: new FormControl(undefined, [Validators.required]),
+      email: new FormControl(undefined, [Validators.required, Validators.email]),
+      password: new FormControl(undefined, [
+        Validators.required,
+        Validators.minLength(8),
+        hasNumberValidator,
+        hasSpecialValidator,
+        hasCapitalValidator,
+        hasSmallValidator,
+      ]),
+      confirmPassword: new FormControl(undefined, [Validators.required]),
+      policy: new FormControl(undefined, [Validators.required]),
+    },
+    { validators: isPasswordMatchValidator }
+  );
+  
+
+  constructor(private _authService: AuthService, private _router: Router, private _cdr: ChangeDetectorRef) { }
+
+  onRegisterFormSubmitted(registerForm: FormGroup): void {
+    if (registerForm.valid) {
+      this._authService
+      .register({
+        email: registerForm.get('email')?.value,
+        password: registerForm.get('password')?.value,
+      })
+      .subscribe({
+        next: () => {
+          this._router.navigateByUrl('/verify')
+        }
+      });
+    } else {
+      this.isValid = true
+    }
+  }
+
+  isInvalid(control: string): boolean {
+    if (this.isValid) return this.registerForm.get(control)!.invalid
+    return this.registerForm.get(control)?.touched ?  this.registerForm.get(control)!.invalid : false
+  }
+
+  navToRegister(): void {
+    this._router.navigate(['/auth/login'])
+  }
 }
